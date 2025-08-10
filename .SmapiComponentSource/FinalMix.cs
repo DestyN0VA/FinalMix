bf1e70dfc7e30f3be9059967fbf226036458cb61
EnderTedi <endertedi2@gmail.com>
2025-08-10 20:00:15 +0200

    ConditionalSwitchEvent impl

--- .SmapiComponentSource/FinalMix.cs
+++ .SmapiComponentSource/FinalMix.cs
@@ -3,6 +3,7 @@ using FinalMix.Util;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using FinalMix.Integrations;
+using StardewValley;
 
 namespace FinalMix;
 
@@ -28,5 +29,46 @@ internal class FinalMix : Mod
         Tokens.CP = Helper.ModRegistry.GetApi<IContentPatcherAPI>("pathoschild.ContentPatcher");
         if (Tokens.CP != null)
             Tokens.RegisterTokens();
+
+
+        //GonnaMoveThisLaterIJustDon'tWannaOrganizeThisYetWithNothingElseImplemented
+        Event.RegisterCommand("DN.SnS_ConditionalSwitchEvent", (@event, args, ctx) =>
+        {
+            if (args.Length < 3)
+                @event.LogCommandErrorAndSkip(args, "event command has less than the required amount of parameters (2).");
+            else
+            {
+                string GSQ = ArgUtility.Get(args, 1);
+                string EventSwitch = ArgUtility.Get(args, 2);
+                string[] commands;
+
+
+                if (!GameStateQuery.CheckConditions(GSQ))
+                {
+                    if (ArgUtility.TryGetOptional(args, 3, out string newEvent, out _, EventSwitch, name: "FalseEvent"))
+                        EventSwitch = newEvent;
+                    else
+                    {
+                        @event.CurrentCommand++;
+                        return;
+                    }
+                }
+
+                string assetName = "Data\\Events\\" + Game1.currentLocation.Name;
+                if (!Game1.content.DoesAssetExist<Dictionary<string, string>>(assetName))
+                {
+                    ctx.LogErrorAndSkip("can't load new event from asset '" + assetName + "' because it doesn't exist");
+                    return;
+                }
+                if (!Game1.content.Load<Dictionary<string, string>>(assetName).TryGetValue(EventSwitch, out var raw2))
+                {
+                    ctx.LogErrorAndSkip($"can't load new event from asset '{assetName}' because it doesn't contain the required '{EventSwitch}' key");
+                    return;
+                }
+                commands = Event.ParseCommands(raw2, ctx.Event.farmer);
+                @event.ReplaceAllCommands(commands);
+                @event.eventSwitched = true;
+            }
+        });
     }
 }
